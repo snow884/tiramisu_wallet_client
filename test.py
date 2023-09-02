e978930a86630c6003be80cf0f7def43aff3b22d
snow884 <adam.ivansky@gmail.com>
2023-09-02 10:48:05 -0400

    add first files

---
+++ test.py
@@ -0,0 +1,72 @@
+from tiramisu_client import TiramisuClient
+import random
+
+# Please go to https://testnet.tarowallet.net/walletapp/ 
+# and create an account then populate your credentials 
+USER_NAME="FILL_THIS_OUT"
+PASSWORD="FILL_THIS_OUT"
+
+print("Create client object")
+client = TiramisuClient(username=USER_NAME,password=PASSWORD)
+
+print("List all wallet balances")
+print( client.balances() )
+
+print("List all balances of NFTs in the wallet")
+print( client.balances_nft() )
+
+print("Get the BTC balance in wallet:")
+print( client.get_btc_balance() )
+
+print("List assets available")
+assets=client.assets()
+print( assets )
+
+print("Get first asset and print it")
+asset=client.asset(4)
+print( asset )
+
+print("List wallet transactions")
+print( client.transactions() )
+
+print("List all currency asset listings on exchange")
+listings = client.listings()
+print( listings )
+
+print(f"Buy asset '{listings[4]['currency']}'")
+print( client.buy_taproot_asset_asset_wait_finished(asset=listings[4]['currency'], amount=1) )
+
+print("Balances after purchase:")
+print( client.balances() )
+
+print("List all NFT asset listings on exchange")
+listings_nfts = client.listings_nfts()
+print( listings_nfts )
+
+print("Show all listings made from the current wallet")
+print( client.listings_my() )
+
+print("transactions_receive_btc")
+transaction_inbound_btc = client.transactions_receive_btc_get_invoice(amount=10000, description='test BTC deposit')
+
+print(f"send BTC to this address to top-up your BTC balance: {transaction_inbound_btc['invoice_inbound']}")
+
+invoice_inbound_taproot_asset = client.transactions_receive_taproot_asset(amount=1, asset=asset['id'], description=f"receive asset {asset['id']}")
+
+print(f"send Taproot asset {asset['name']} with ID {asset['id']} to this address to top-up your TAP balance: {invoice_inbound_taproot_asset}")
+
+tn = random.randint(1, 1000)
+
+print("Minting a new currency and waiting for minting to finish.")
+
+new_asset = client.assets_mint_wait_finished(acronym=f'TC{tn}', name=f'Testasset{tn}', description=f'Test asset {tn}', supply=1234, file_path='test_image.jpg')
+
+print("Newly created asset")
+print(new_asset)
+
+print("Listing the currency on an exchange...")
+client.list_asset(asset=new_asset['id'])
+
+print("Show all listings made from the current wallet")
+print( client.listings_my() )
+
